0a00cd4d9898f36c4a22977df4a7dab551fced21
sijuki09 <163381776+sijuki09@users.noreply.github.com>
2024-06-03 21:14:41 +0700

    Update client.c

--- client.c
+++ client.c
@@ -1,57 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
-#define SERVER_IP "127.0.0.1"
 
-int main() {
-    int sock = 0;
+void send_request(const char *message) {
+    int sock = 0, valread;
     struct sockaddr_in serv_addr;
     char buffer[1024] = {0};
-
-    // Create socket
+    
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
-        return -1;
+        return;
     }
-
+    
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+    
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
-        return -1;
+        return;
     }
-
-    // Connect to server
+    
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("\nConnection Failed \n");
-        return -1;
+        return;
     }
-
-    // Send POST commands and numbers
-    send(sock, "POST", strlen("POST"), 0);
-    send(sock, "5", strlen("5"), 0);
-    printf("POST 5\n");
-
-    send(sock, "POST", strlen("POST"), 0);
-    send(sock, "3", strlen("3"), 0);
-    printf("POST 3\n");
-
-    // Send GET command
-    send(sock, "GET", strlen("GET"), 0);
-    printf("GET\n");
-
-    // Receive response from server
-    read(sock, buffer, 1024);
-    printf("%s\n", buffer);
-
+    
+    send(sock, message, strlen(message), 0);
+    valread = read(sock, buffer, 1024);
+    printf("%s", buffer);
     close(sock);
+}
+
+int main(int argc, char const *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <COMMAND> [ARG]\n", argv[0]);
+        return 1;
+    }
+    
+    if (strcmp(argv[1], "POST") == 0 && argc == 3) {
+        char message[1024];
+        snprintf(message, sizeof(message), "POST %s", argv[2]);
+        send_request(message);
+    } else if (strcmp(argv[1], "GET") == 0) {
+        send_request("GET");
+    } else {
+        printf("Usage: %s <COMMAND> [ARG]\n", argv[0]);
+    }
+    
     return 0;
 }
